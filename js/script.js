11b3526f06ad43280ad42bd8608d8512ceaf9ce1
Kost9In <kost9inbi4@mail.ru>
2016-07-27 18:16:03 +0300

    fix bugs

--- js/script.js
+++ js/script.js
@@ -1,17 +1,66 @@
 
 document.addEventListener('DOMContentLoaded', function(event) {
 
+  const qs = selector => document.querySelector(selector);
+
   const todo = new Todo({
+    items: (localStorage.todoItems) ? JSON.parse(localStorage.todoItems) : [],
     listWrapper: '.todo-wrapper .todo-list ul',
-    actionsWrapper: '.todo-actions',
-    addInput: 'form.add-todo input[name="add-todo"]',
-    searchInput: 'form.todo-search input[name="todo-search"]',
-    filterInfo: '.todo-wrapper .todo-filter'
+    onSelect: (selected) => { // select items
+      const actionsWrapper = qs('.todo-actions');
+      if (selected.length) {
+        actionsWrapper.classList.add('show');
+        if (selected.length > 1) actionsWrapper.classList.add('many');
+        else actionsWrapper.classList.remove('many');
+      }
+      else actionsWrapper.classList.remove('show');
+    },
+    onChange: (items) => { // save items
+      localStorage.todoItems = JSON.stringify(items);
+    }
   });
 
-  document.querySelector('.todo-wrapper form.add-todo').addEventListener('submit', todo.add.bind(todo));
-  document.querySelector('.todo-actions .edit').addEventListener('click', todo.edit.bind(todo));
-  document.querySelector('.todo-actions .remove').addEventListener('click', todo.remove.bind(todo));
-  document.querySelector('.todo-wrapper form.todo-search').addEventListener('submit', todo.filter.bind(todo));
+  /* add item */
+  qs('.todo-wrapper form.add-todo').addEventListener('submit', e => {
+    e.preventDefault();
+    const input = e.currentTarget.querySelector('input[name="add-todo"]');
+    todo.add.call(todo, input.value.trim());
+    input.value = '';
+  });
+
+  /* edit items */
+  qs('.todo-actions .edit').addEventListener('click', e => {
+    e.preventDefault();
+    todo.edit.call(todo);
+  });
+
+  /* remove items */
+  qs('.todo-actions .remove').addEventListener('click', e => {
+    e.preventDefault();
+    todo.remove.call(todo);
+  });
+
+  /* filter items */
+  qs('.todo-wrapper form.todo-search').addEventListener('submit', e => {
+    e.preventDefault();
+    const searchInput = qs('form.todo-search input[name="todo-search"]');
+    const filterText = searchInput.value.trim();
+    todo.filter.call(todo, filterText);
+    const filterInfo = qs('.todo-wrapper .todo-filter');
+    if (filterText) {
+      filterInfo.innerHTML = `Filter: ${filterText}`;
+      const remove = document.createElement('a');
+      remove.classList = 'remove';
+      remove.innerHTML = '<i class="fa fa-times"></i>';
+      remove.addEventListener('click', e => {
+        e.preventDefault();
+        searchInput.value = '';
+        filterInfo.innerHTML = '';
+        todo.filter.call(todo, '');
+      });
+      filterInfo.appendChild(remove);
+    }
+    else filterInfo.innerHTML = '';
+  });
 
 });
